e3206e393707ccc059613b632c7668caa1f6556a
Ahkal <bp@sojourn.llc>
2022-06-26 21:31:17 -0400

    Interview practice, closures using let and const

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
- function makeFun(){
-  let privateNum = 0;
-  return {
-    logNum: () => console.log(privateNum),
-    increment: () => {
-      privateIncrement();
-      console.log('Incremented!');
-    },
+// VAR - i is global scope
+// for (var i = 0; i < 3; i++) {
+//   const log = () => {
+//     debugger;
+//     console.log(i); // 3,3,3
+//   }
+//   setTimeout(log, 100)
+// }
+
+// LET - i is block scope
+for (let i = 0; i < 3; i++) {
+  const log = () => {
+    // debugger;
+    console.log(i); // 0,1,2
   }
- }
+  setTimeout(log, 1000)// After 1sec, shows all i's.
+}
